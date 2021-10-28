6bd0ed216f5c3565a40b05611a71887972cacfad
inbal levi <inbalevi2014@gmail.com>
2021-10-28 12:38:39 +0300

    adding staging

---
+++ Staging/modules/vm/variables.tf
@@ -0,0 +1,35 @@
+variable "location" {
+  description = "Region to build into"
+  default     = "westeurope"
+
+}
+
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+  default     = "Terraform_Resource_Group"
+}
+
+variable "public_vm_size" {
+  type        = string
+  description = "Vm-Size Config"
+  default     = "Standard_B1ls"
+}
+
+variable "network_interface_ids"{
+  type        = list(string)
+  default = ["azurerm_network_interface.nic.id", "azurerm_network_interface.nic2.id", "azurerm_network_interface.nic3.id"]
+  
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Administrator user name for virtual machine"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Password must meet Azure complexity requirements"
+}
+
